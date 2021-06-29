bafc7ddafb8e4ce31e58e0adba4b336fabef4fad
AnushaAnanthan <anuananthan98@gmail.com>
2021-06-29 15:29:57 +0530

    All files are added

--- Myclient.java
+++ Myclient.java
@@ -9,12 +9,14 @@ DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 String str= " ";
 String str2=" ";
-while(!str.equals("stop")){
-str=br.readLine();
-dout.writeUTF(str);
-dout.flush();
-str2=din.readUTF();
-System.out.println("server says :"+str2);
+while(!str.equals("stop"))
+{
+     str=br.readLine();
+     System.out.println("Echo :"+str);
+     dout.writeUTF(str);
+     dout.flush();
+     str2=din.readUTF();
+     System.out.println("server says :"+str2);
 }
 dout.close();
 s.close();
